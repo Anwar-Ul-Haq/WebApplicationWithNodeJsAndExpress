6484f9c684be1753ff51a4eb8af02dc6c4bae9ba
Anwar Ul Haq <aulhaq6@gmail.com>
2021-10-23 20:28:46 +0100

    Template Engine and  added website template

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ app.use(morgan('combined'));
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
 
-    res.send('Hello from my app')
+    res.render('index', { title: 'Anwar demo site', data:['a','b','c']});
     
 });
 
